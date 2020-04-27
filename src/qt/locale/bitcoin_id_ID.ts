db6e149d0fd39e8fe7e0530ed666013d6d1d2fa4
imskaa <nestchain.org@gmail.com>
2020-04-27 08:28:55 +0000

    Update bitcoin_id_ID.ts

--- src/qt/locale/bitcoin_id_ID.ts
+++ src/qt/locale/bitcoin_id_ID.ts
@@ -1856,8 +1856,8 @@
 <context>
     <name>bitcoin-core</name>
     <message>
-        <source>Litecoin Core</source>
-        <translation>Litecoin Core</translation>
+        <source>UbiState Core</source>
+        <translation>UbiState Core</translation>
     </message>
     <message>
         <source>Corrupted block database detected</source>
