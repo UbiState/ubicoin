b5f5889f16553f08a75b4900491d19a139f3ddf3
imskaa <nestchain.org@gmail.com>
2020-04-27 08:30:48 +0000

    Update bitcoin_ta.ts

--- src/qt/locale/bitcoin_ta.ts
+++ src/qt/locale/bitcoin_ta.ts
@@ -712,7 +712,7 @@
 <context>
     <name>bitcoin-core</name>
     <message>
-        <source>Litecoin Core</source>
+        <source>UbiState Core</source>
         <translation>Litecoin மையம்</translation>
     </message>
     <message>
