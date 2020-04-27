94271b59955280be6dce967100fed5795e30ae26
imskaa <nestchain.org@gmail.com>
2020-04-27 08:27:11 +0000

    Update bitcoin_de.ts

--- src/qt/locale/bitcoin_de.ts
+++ src/qt/locale/bitcoin_de.ts
@@ -3176,8 +3176,8 @@ Hinweis: Eine Gebühr von "100 Satoshis pro kB" bei einer Größe der Transaktio
         <translation>Kann HTTP Server nicht starten. Siehe debug log für Details.</translation>
     </message>
     <message>
-        <source>Litecoin Core</source>
-        <translation>Litecoin Core</translation>
+        <source>UbiState Core</source>
+        <translation>UbiState Core</translation>
     </message>
     <message>
         <source>The %s developers</source>
