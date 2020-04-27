468c2648f028474260cc0b876826561a4f91d728
imskaa <nestchain.org@gmail.com>
2020-04-27 08:23:39 +0000

    Update test_runner.py

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -312,8 +312,8 @@ def run_tests(test_list, src_dir, build_dir, tmpdir, jobs=1, enable_coverage=Fal
 
     # Warn if bitcoind is already running (unix only)
     try:
-        if subprocess.check_output(["pidof", "litecoind"]) is not None:
-            print("%sWARNING!%s There is already a litecoind process running on this system. Tests may fail unexpectedly due to resource contention!" % (BOLD[1], BOLD[0]))
+        if subprocess.check_output(["pidof", "UbiServer"]) is not None:
+            print("%sWARNING!%s There is already a UbiServer process running on this system. Tests may fail unexpectedly due to resource contention!" % (BOLD[1], BOLD[0]))
     except (OSError, subprocess.SubprocessError):
         pass
 
