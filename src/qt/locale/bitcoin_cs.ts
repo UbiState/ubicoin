b855f196f921f754ea5d08dc7b79b0596b26e848
imskaa <nestchain.org@gmail.com>
2020-04-27 08:26:52 +0000

    Update bitcoin_cs.ts

--- src/qt/locale/bitcoin_cs.ts
+++ src/qt/locale/bitcoin_cs.ts
@@ -3062,8 +3062,8 @@
         <translation>Nemohu spustit HTTP server. Detaily viz v debug.log.</translation>
     </message>
     <message>
-        <source>Litecoin Core</source>
-        <translation>Litecoin Core</translation>
+        <source>UbiState Core</source>
+        <translation>UbiState Core</translation>
     </message>
     <message>
         <source>The %s developers</source>
