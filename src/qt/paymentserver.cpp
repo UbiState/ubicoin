f0d8a4c7834d15da65e6a3e29929a17fa13e36b9
imskaa <nestchain.org@gmail.com>
2020-04-27 09:59:43 +0000

    Update paymentserver.cpp

--- src/qt/paymentserver.cpp
+++ src/qt/paymentserver.cpp
@@ -75,7 +75,7 @@ namespace // Anon namespace
 //
 static QString ipcServerName()
 {
-    QString name("UbicoinQt");
+    QString name("UbiWallet");
 
     // Append a simple hash of the datadir
     // Note that GetDataDir(true) returns a different path
