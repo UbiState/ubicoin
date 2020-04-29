7a46eef0b8cf89cbadad04eab7232206944b54b2
MonkeyD-Core <62377315+MonkeyD-Core@users.noreply.github.com>
2020-04-29 11:49:44 +0000

    Add Seed Node

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -129,11 +129,8 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed-a.ubicoin.loshan.co.uk");
-        vSeeds.emplace_back("dnsseed.thrasher.io");
-        vSeeds.emplace_back("dnsseed.ubicointools.com");
-        vSeeds.emplace_back("dnsseed.ubicoinpool.org");
-        vSeeds.emplace_back("dnsseed.koin-project.com");
+        vSeeds.emplace_back("52.136.119.221");
+        vSeeds.emplace_back("51.143.38.195");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,68);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
