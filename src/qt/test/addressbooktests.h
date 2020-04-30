45af5703899e398c8f46a13d46d5dd81648d284e
imskaa <nestchain.org@gmail.com>
2020-04-30 12:40:31 +0000

    Create addressbooktests.h

---
+++ src/qt/test/addressbooktests.h
@@ -0,0 +1,15 @@
+#ifndef UBICOIN_QT_TEST_ADDRESSBOOKTESTS_H
+#define UBICOIN_QT_TEST_ADDRESSBOOKTESTS_H
+
+#include <QObject>
+#include <QTest>
+
+class AddressBookTests : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void addressBookTests();
+};
+
+#endif // UBICOIN_QT_TEST_ADDRESSBOOKTESTS_H
