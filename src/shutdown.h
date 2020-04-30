6362a21979233ab4492013e7de0fad29ee472d2a
imskaa <nestchain.org@gmail.com>
2020-04-30 12:51:11 +0000

    Create shutdown.h

---
+++ src/shutdown.h
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 Isaac
+// Copyright (c) 2020 The Ubicoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef UBICOIN_SHUTDOWN_H
+#define UBICOIN_SHUTDOWN_H
+
+void StartShutdown();
+void AbortShutdown();
+bool ShutdownRequested();
+
+#endif
